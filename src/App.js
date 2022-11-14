cbca2f45e77df0d39f560a41639fbd1c7c2d7462
David Agbaeze <you@example.com>
2022-11-14 06:05:43 +0100

    solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,125 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { CartProvider } from './components/Context/CartContext';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './style/App.scss';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import CategoryPage from './components/CategoryPage/CategoryPage';
+import ProductPage from './components/ProductPage/ProductPage';
+import ErrorPage from './components/Utils/ErrorPage/ErrorPage';
+import Header from './components/Utils/Header/Header';
+import CartPage from './components/CartPage/CartPage';
+import { Query } from '@apollo/react-components';
+import { getAllCategories } from "./queries/getQueries";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currencyKey: 0,
+    };
+  }
+
+  componentDidMount() {
+    const currencyDropdown = document.querySelector('.dropdown-text');
+
+    document.addEventListener('click', () => {
+      switch (currencyDropdown.textContent.charAt(0)) {
+        case '£':
+          this.setState({
+            currencyKey: 1,
+          });
+          break;
+        case 'A':
+          this.setState({
+            currencyKey: 2,
+          });
+          break;
+        case '¥':
+          this.setState({
+            currencyKey: 3,
+          });
+          break;
+        case '₽':
+          this.setState({
+            currencyKey: 4,
+          });
+          break;
+        default:
+          this.setState({
+            currencyKey: 0,
+          });
+      }
+    });
+  }
+
+  render() {
+    const { currencyKey } = this.state;
+    return (
+      <div className='App'>
+        <CartProvider>
+          <Header />
+          <Query query={getAllCategories}>
+            {({ loading, data }) => {
+              if (loading) {
+                return;
+              } else {
+                const { categories } = data;
+                return (
+                  <Routes>
+                    <Route
+                      path='/sw-erd-test'
+                      element={<Navigate to='/sw-erd-test/all' />}
+                    />
+                    <Route
+                      path={`/sw-erd-test/${categories[0].name}`}
+                      element={
+                        <CategoryPage
+                          currency={currencyKey}
+                          categoryName={categories[0].name}
+                        />
+                      }
+                    />
+                    <Route
+                      path={`/sw-erd-test/${categories[1].name}`}
+                      element={
+                        <CategoryPage
+                          currency={currencyKey}
+                          categoryName={categories[1].name}
+                        />
+                      }
+                    />
+                    <Route
+                      path={`/sw-erd-test/${categories[2].name}`}
+                      element={
+                        <CategoryPage
+                          currency={currencyKey}
+                          categoryName={categories[2].name}
+                        />
+                      }
+                    />
+
+                    <Route
+                      path='/sw-erd-test/product/:productID'
+                      element={
+                        <ProductPage category='0' currency={currencyKey} />
+                      }
+                    />
+                    <Route
+                      exact
+                      path='/sw-erd-test/cart'
+                      element={<CartPage currency={currencyKey} />}
+                    />
+                    <Route path='*' element={<ErrorPage />} />
+                  </Routes>
+                );
+              }
+            }}
+          </Query>
+        </CartProvider>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './style/index.scss';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { BrowserRouter } from 'react-router-dom';
+
+// Apollo client setup
+const GRAPHQL_URI = 'http://localhost:4000/'
+const client = new ApolloClient({
+  uri: GRAPHQL_URI,
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ApolloProvider>
+  </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
